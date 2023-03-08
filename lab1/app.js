11ffb85af23a35fa05eb6cdc9ba6d2c9369dcdc0
sumtvpd08179@fpt.edu.vn <sumtvpd08179@fpt.edu.vn>
2023-03-08 13:38:53 +0700

    nop lab 1

---
+++ lab1/app.js
@@ -0,0 +1,10 @@
+// Nhập vào hai số nguyên a và b từ bàn phím
+let a = parseInt(prompt("Nhập vào số nguyên a:"));
+let b = parseInt(prompt("Nhập vào số nguyên b:"));
+
+// Tính tổng c của a và b
+let c = a + b;
+
+// In ra kết quả bằng hai cách
+document.write("Tổng là " + c);
+console.log("Tổng là " + c);
