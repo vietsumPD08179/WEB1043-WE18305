4c5d49fd4fab5dfa7f0bd6bd4591a8856c07fc91
sumtvpd08179@fpt.edu.vn <sumtvpd08179@fpt.edu.vn>
2023-03-27 12:30:25 +0700

    nop lab4

--- lab4/bai1.js
+++ lab4/bai1.js
@@ -40,7 +40,7 @@ class Student {
 
 // Tạo danh sách sinh viên
 const students = [
-  new Student(1, "Tran", "Viet Sum", "A01", "sum@gmail.com", "0123456789", "2004-01-01"), 
+  new Student(1, "Tran", "Viet Sum", "A01", "sum@gmail.com", "0123456789", "2004-01-01",),
   new Student(2, "Nguyen", "The Hau", "B01", "hau@gmail.com", "0123456789", "2004-01-02"),
   new Student(3, "Doan", "Anh Quy", "C02", "quy@gmail.com", "0123456789", "2004-01-03"),
   new Student(4, "Tran", "Van Nhan", "D02", "Nhan@gmail.com", "0123456789", "2004-01-04"),
@@ -51,10 +51,9 @@ const students = [
 students.sort((a, b) => a.getAgeByDay() - b.getAgeByDay());
 
 // In ra danh sách sinh viên đã sắp xếp
-document.write("Danh sách sinh viên theo thứ tự tuổi từ thấp đến cao:");
-students.forEach((student) =>
-document.write(`${student.getFullName()} - Tuổi: ${student.getAgeByDay()} ngày`)
-);
-
+document.writeln("Danh sách sinh viên theo thứ tự tuổi từ thấp đến cao:"); 
+document.writeln("<br>")
+students.forEach((student) => document.writeln(`${student.getFullName()} - Tuổi: ${student.getAgeByDay()} ngày`+"<br>") );
+document.writeln("<br>")
 // In ra width và height của màn hình
-document.write(`Width: ${window.innerWidth}px - Height: ${window.innerHeight}px`);
+document.writeln(`Width: ${window.innerWidth}px - Height: ${window.innerHeight}px`);
