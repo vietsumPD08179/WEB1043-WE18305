94166822540c43e21a0f4f7b002b7e3964068825
sumtvpd08179@fpt.edu.vn <sumtvpd08179@fpt.edu.vn>
2023-04-09 16:44:43 +0700

    assgiment 2

--- assignment1/index.js
+++ assignment1/index.js
@@ -1,28 +1,60 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+//banner
+var counter = 1;
+setInterval(function () {
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 5000);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+
+
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#name');
+
+form.addEventListener('submit', (event) => {
+  if (nameInput.value === '') {
+    event.preventDefault();
+    alert('Vui lòng nhập họ và tên');
+  }
+});
+
+const phoneInput = document.querySelector('#phone');
+
+form.addEventListener('submit', (event) => {
+  const phoneRegex = /^0\d{9}$/;
+  if (!phoneRegex.test(phoneInput.value)) {
+    event.preventDefault();
+    alert('Số điện thoại không hợp lệ');
+  }
+});
+const emailInput = document.querySelector('#email');
+
+form.addEventListener('submit', (event) => {
+  const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (!emailRegex.test(emailInput.value)) {
+    event.preventDefault();
+    alert('Email không hợp lệ');
+  }
+});
+
+form.addEventListener('submit', (event) => {
+  if (phoneInput.value !== '' && emailInput.value === '') {
+    event.preventDefault();
+  }
+});
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
 }
